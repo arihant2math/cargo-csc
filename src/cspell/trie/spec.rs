2b00161fde059128ee0873a2eafe4ad9960fd774
Ashwin Naren <arihant2math@gmail.com>
2025-05-08 12:28:01 -0700

    somewhat working

--- src/cspell/trie/spec.rs
+++ src/cspell/trie/spec.rs
@@ -4,9 +4,7 @@ use std::{
     cell::RefCell,
     collections::HashMap,
     io::Read,
-    ops::Deref,
     rc::Rc,
-    sync::{Arc, Mutex},
 };
 
 #[derive(Debug)]
@@ -63,15 +61,19 @@ fn parse_header(input: &[String]) -> anyhow::Result<(usize, Header)> {
 
 struct TrieNode {
     eow: bool,
+    parent: Option<Rc<RefCell<TrieNode>>>,
     children: HashMap<char, Rc<RefCell<TrieNode>>>,
 }
+
 /// Internal parse states.
 #[derive(Debug)]
 enum ParseState {
     InWord,
     Escape,
     Remove,
-    InId { chars: Vec<char> },
+    AbsoluteReference { chars: Vec<char> },
+    // TODO: impl
+    // RelRef { chars: Vec<char> },
 }
 
 /// Helper struct that builds a trie.
@@ -84,43 +86,141 @@ struct TrieBuilder {
 
 impl TrieBuilder {
     fn new() -> Self {
-        let root = Rc::new(RefCell::new(TrieNode::new(false)));
+        let root = Rc::new(RefCell::new(TrieNode::new_root()));
         Self {
             nodes: vec![root.clone()],
             pos: vec![root],
         }
     }
 
+    fn dbg_state(&self) {
+        fn bstr(b: bool) -> String {
+            if b { "*".to_string() } else { " ".to_string() }
+        }
+        for (i, node) in self.nodes.iter().enumerate() {
+            let pos_pos = self
+                .pos
+                .iter()
+                .position(|p| Rc::ptr_eq(&p, &node))
+                .map(|p| p as i64)
+                .unwrap_or(-1);
+            let mut c = self.get_char(node);
+            let node_borrow = node.borrow();
+            let mut child_ids: Vec<_> = node_borrow
+                .children
+                .values()
+                .map(|v| {
+                    // Find position in nodes
+                    self.nodes
+                        .iter()
+                        .position(|p| Rc::ptr_eq(&p, &v))
+                        .unwrap_or(usize::MAX)
+                })
+                .collect();
+            child_ids.sort();
+            let children = child_ids
+                .iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<_>>()
+                .join(",");
+            println!(
+                "{pos_pos:>2} {c} ID {:>3}: {} children={}",
+                i,
+                bstr(node_borrow.eow),
+                children
+            );
+        }
+    }
+
+    fn get_char(&self, node: &Rc<RefCell<TrieNode>>) -> char {
+        // Get parent
+        let node_borrow = node.borrow();
+        let parent = node.borrow().parent.clone();
+        if let Some(parent) = parent {
+            let parent_borrow = parent.borrow();
+            for (c, child) in &parent_borrow.children {
+                if Rc::ptr_eq(&child, node) {
+                    return *c;
+                }
+            }
+        }
+        ' '
+    }
+
+    /// Absolute jump to a node in the trie.
+    fn jump_to(&mut self, idx: usize) {
+        // 1) Peek, don’t pop:
+        let last = self.pos.last().unwrap().clone();
+
+        // 2) Get the char and parent:
+        let ch = self.get_char(&last);
+        let parent = last.borrow().parent.clone().unwrap();
+
+        // 3) Redirect that slot, _in-place_:
+        parent.borrow_mut().children.insert(ch, Rc::clone(&self.nodes[idx]));
+
+        // 4) Replace the top of the stack with the referenced node:
+        *self.pos.last_mut().unwrap() = Rc::clone(&self.nodes[idx]);
+    }
+
     /// Process a single character and update state.
     fn process_char(&mut self, c: char, header_base: u32, state: &mut ParseState) {
+        dbg!(c, &state);
+        self.dbg_state();
         match state {
             ParseState::Escape => {
-                *state = ParseState::InWord;
                 self.add_char(c);
+                *state = ParseState::InWord;
             }
             ParseState::Remove => {
                 let count = if c.is_numeric() {
-                    c.to_digit(10).unwrap_or(1)
+                    let out = c.to_digit(10).unwrap();
+                    // As per the spec, out can't be 1
+                    assert_ne!(out, 1);
+                    out
                 } else {
                     1
                 };
                 for _ in 0..count {
-                    self.pos.pop();
+                    if self.pos.pop().is_none() {
+                        self.dbg_state();
+                        panic!("No more nodes to pop");
+                    }
                 }
-                *state = ParseState::InWord;
+                if count == 1 {
+                    match c {
+                        '\\' => *state = ParseState::Escape,
+                        '$' => {
+                            if let Some(cur) = self.pos.last() {
+                                cur.borrow_mut().eow = true;
+                            }
+                            *state = ParseState::Remove;
+                        }
+                        '<' => *state = ParseState::Remove,
+                        '#' => {
+                            *state = ParseState::AbsoluteReference { chars: vec![c] };
+                        }
+                        _ => {
+                            self.add_char(c);
+                            *state = ParseState::InWord;
+                        },
+                    }
+                }
             }
-            ParseState::InId { chars } => {
-                if c.is_numeric() {
-                    chars.push(c);
-                } else if c == ';' {
+            ParseState::AbsoluteReference { chars } => {
+                if c == ';' {
                     let number_str: String = chars.iter().collect();
-                    let idx = u32::from_str_radix(&number_str, header_base)
+                    let idx = u32::from_str_radix(&number_str[1..], header_base)
                         .expect("Failed to convert number") as usize;
                     if idx < self.nodes.len() {
-                        let node = self.nodes[idx].clone();
-                        self.pos = vec![node];
+                        self.jump_to(idx);
+                    } else {
+                        self.dbg_state();
+                        panic!("Index out of bounds: {}", idx);
                     }
                     *state = ParseState::InWord;
+                } else {
+                    chars.push(c);
                 }
             }
             ParseState::InWord => match c {
@@ -129,10 +229,11 @@ impl TrieBuilder {
                     if let Some(cur) = self.pos.last() {
                         cur.borrow_mut().eow = true;
                     }
+                    *state = ParseState::Remove;
                 }
                 '<' => *state = ParseState::Remove,
-                _ if c.is_numeric() => {
-                    *state = ParseState::InId { chars: vec![c] };
+                '#' => {
+                    *state = ParseState::AbsoluteReference { chars: vec![c] };
                 }
                 _ => self.add_char(c),
             },
@@ -146,22 +247,45 @@ impl TrieBuilder {
             if let Some(child) = parent_borrow.children.get(&c) {
                 self.pos.push(child.clone());
             } else {
-                let new_node = Rc::new(RefCell::new(TrieNode::new(false)));
+                // TODO: causes leak
+                let new_node = Rc::new(RefCell::new(TrieNode::new(parent.clone(), false)));
                 parent_borrow.children.insert(c, new_node.clone());
                 self.nodes.push(new_node.clone());
                 self.pos.push(new_node);
             }
+        } else {
+            self.dbg_state();
+            unreachable!();
         }
     }
 }
 
 impl TrieNode {
     /// Create a new TrieNode.
-    fn new(eow: bool) -> Self {
+    fn new(parent: Rc<RefCell<Self>>, eow: bool) -> Self {
         TrieNode {
             eow,
             children: HashMap::new(),
+            parent: Some(parent),
         }
+    }
+
+    fn new_root() -> Self {
+        TrieNode {
+            eow: false,
+            children: HashMap::new(),
+            parent: None,
+        }
+    }
+
+    fn deep_clone(&self) -> Self {
+        let mut new_node = TrieNode::new_root();
+        new_node.eow = self.eow;
+        for (c, child) in &self.children {
+            let child_clone = child.borrow().deep_clone();
+            new_node.children.insert(*c, Rc::new(RefCell::new(child_clone)));
+        }
+        new_node
     }
 }
 
@@ -207,9 +331,6 @@ pub fn parse_body(input: &[String], header: &Header) -> crate::Trie {
 pub fn parse_trie(input: &[String]) -> anyhow::Result<(Header, crate::Trie)> {
     let (counter, header) = parse_header(input)?;
     let body = &input[counter..];
-    dbg!(&body[0]);
-    dbg!(&body[1]);
-    dbg!(&body[2]);
     let trie = parse_body(body, &header);
     Ok((header, trie))
 }
@@ -238,7 +359,7 @@ pub fn file_to_lines<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<Vec<
 mod tests {
     use super::*;
 
-    #[test]
+    // #[test]
     fn test_parse_header() {
         let input = vec![
             "TrieXv4".to_string(),
@@ -251,8 +372,8 @@ mod tests {
         assert_eq!(header.base, 10);
     }
 
-    #[test]
-    fn test_parse_body() {
+    // #[test]
+    fn test_parse_body_basic() {
         let header = Header {
             version: Version("TrieXv4".to_string()),
             base: 10,
@@ -260,20 +381,34 @@ mod tests {
         let input = vec!["a$".to_string(), "b$".to_string(), "c$".to_string()];
         let trie = parse_body(&input, &header);
         assert!(trie.contains("a"));
-        assert!(!trie.contains("b"));
-        assert!(!trie.contains("c"));
+        assert!(trie.contains("b"));
+        assert!(trie.contains("c"));
         assert!(!trie.contains("d"));
-        assert!(trie.contains("ab"));
-        assert!(trie.contains("abc"));
+        assert!(!trie.contains("ab"));
+        assert!(!trie.contains("abc"));
     }
 
     #[test]
+    fn test_small() {
+        let path = r"D:\Documents\Programming\cargo-csc\test.trie";
+        let lines = file_to_lines(path).unwrap();
+        let (header, trie) = parse_trie(&lines).unwrap();
+        let v = trie.to_vec();
+        for word in &v {
+            println!("{}", word);
+        }
+        assert_eq!(header.version.to_u8(), 3);
+        assert!(v.contains(&"'cause".to_string()));
+    }
+
+    // #[test]
     fn test_parse_en_us() {
         let path =
             r"C:\Users\ariha\.code-spellcheck\tmp\cspell-dicts\dictionaries\en_US\en_US.trie";
         let lines = file_to_lines(path).unwrap();
         let (header, trie) = parse_trie(&lines).unwrap();
-        dbg!(trie);
+        dbg!(&trie.to_vec());
         assert_eq!(header.version.to_u8(), 3);
+        assert!(trie.contains("'cause'"))
     }
 }
